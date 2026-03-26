4856f09e7a1135f66ff9b41af613e850e7e7ece1
Rex Shrestha <0shrer13@solent.ac.uk>
2026-03-26 15:42:45 +0000

    Everything

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,30 @@
 export default function App() {
-    return <h1>Welcome to the HitTastic! App!</h1>;
+    return <h1>Rex</h1>;
 }
+import { useState, useEffect } from "react";
+
+const [username , setUsername] = useState<string | null>(null);
+const [inputUser, setInputUser] = useState("")
+const [inputPass, setInputPass] = useState("")
+
+const login = async () => {
+  const res = await fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      
+    },
+    credentials: "include",
+    body: JSON.stringify({
+      username: inputUser,
+      password: inputPass,
+    }),
+  });
+  if (res.status === 200) {
+    const data = await res.json();
+    setUsername(data.username);
+  } else {
+    alert("Login failed");
+    setUsername(null);
+  }
+};
